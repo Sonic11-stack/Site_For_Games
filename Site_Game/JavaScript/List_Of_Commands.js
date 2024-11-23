f68781fa0213f17ec9e2bad0dfe04918e78ab8ad
SUS <konnorrk850@gmail.com>
2024-11-23 15:00:49 +0300

    Change form of The Third page (from bootstrap to grid)

---
+++ Site_Game/JavaScript/List_Of_Commands.js
@@ -0,0 +1,34 @@
+        function redirectToNews() {
+            window.location.href='../HTML/News.html';
+        }
+
+        function redirectToTheLastOfUs() {
+            window.location.href='../HTML/The Last Of Us.html';
+        }
+
+        function redirectToFirstPage() {
+            window.location.href='../HTML/First_Page.html';
+        }
+
+        function redirectToSecondPage() {
+            window.location.href='../HTML/Second_Page.html';
+        }
+
+        function redirectToThirdPage() {
+            window.location.href='../HTML/Third_Page.html';
+        }
+
+        function redirectToGames() {
+            window.location.href='../HTML/Games.html';
+        }
+
+        function redirectToGameEngines() {
+            window.location.href='../HTML/Game_Engines.html';
+        }
+        
+        function redirectToPageTelegram() {
+            window.location.href='https://t.me/Fetyper';
+        }
+        function redirectToVK() {
+            window.location.href='https://vk.com/coolstudent1';
+        }
